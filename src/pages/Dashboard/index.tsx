1a04850595c64ab6068e15339d81a9f03b96065b
Alexandre Souza <sp.alexandre26@gmail.com>
2022-02-20 09:59:31 -0300

    Conexão com  API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,65 @@
-import React from "react";
-import { FiChevronRight} from 'react-icons/fi'
+import React, { useState, FormEvent } from "react";
+import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
+import api from "../../services/api";
+import Repository from "../Repository";
 import { Title, Form, Repositories } from './style'
 
+
+interface Repository{
+    full_name: string;
+    login: string;
+    owner: {
+        avatar_url: string;
+        description: string;
+    };
+}
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, newRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement> ): Promise<void> {
+        event.preventDefault();
+        
+        const response = await api.get(`repos/${newRepo}`)
+
+        console.log(response.data);
+
+        const repository = response.data;
+
+        newRepositories([...repositories, repository]);
+
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="WareHouse Explorer" />
             <Title>Explore repositorios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite Aqui" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={e => setNewRepo(e.target.value)}
+                    placeholder="Digite Aqui" />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
                     <img
-                        src="https://avatars.githubusercontent.com/u/55266497?v=4"
-                        alt="Alexandre Souza"
+                        src={repository.owner.avatar_url}
+                        alt={repository.login}
                     />
                     <div>
-                        <strong>rocketseat/unform</strong>
-                        <p> Teste de criação do Gabiru </p>
+                        <strong>{repository.full_name}</strong>
+                        <p> {repository.owner.description}</p>
                     </div>
                     <FiChevronRight size={20} />
                 </a>
-                <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/55266497?v=4"
-                        alt="Alexandre Souza"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p> Teste de criação do Gabiru </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/55266497?v=4"
-                        alt="Alexandre Souza"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p> Teste de criação do Gabiru </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
+                ))}
             </Repositories>
         </>
     );
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -77,7 +77,8 @@ export const Repositories = styled.div`
         }
 
         div {
-            margin-left: 14px;
+            margin: 0 16px;
+            flex: 1;
             strong{
                 font-size: 20px;
                 color: #3d3d4d;    
