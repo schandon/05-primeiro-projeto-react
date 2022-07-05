c841ac863c466720799012440119e7b4e7dff758
schandon <sp.alexandre26@gmail.com>
2022-07-05 17:05:13 -0300

    Codando Container Repository Arrumação

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from "react-router-dom";
+
 
 import logoImg from '../../assets/logo.svg';
 import api from "../../services/api";
@@ -18,7 +20,19 @@ interface Repository{
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+        if (storageRepositories) {
+            return JSON.parse(storageRepositories);
+        }
+        return []; 
+    });
+
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
@@ -55,7 +69,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                     <img
                         src={repository.owner.avatar_url}
                         alt={repository.login}
@@ -65,7 +79,7 @@ const Dashboard: React.FC = () => {
                         <p> {repository.owner.description}</p>
                     </div>
                     <FiChevronRight size={20} />
-                </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Link, useRouteMatch } from "react-router-dom";
+import logoimg from "../../assets/logo.svg"
+interface RepositoryParams{
+    repository: string;
+}
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <img src={logoimg} alt="github_explorer"></img>
+        <Link to="/">
+            <FiChevronLeft size={16}>
+            Voltar        
+        </Link>
+    
+    );
 
 };
  
